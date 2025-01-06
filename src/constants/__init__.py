6c7dccb3a7b2875f589ea059ee882369e0a68249
raunakgupta123 <dhruvgupta26082006@gmail.com>
2025-01-06 20:54:13 +0530

    completed

---
+++ src/constants/__init__.py
@@ -0,0 +1,5 @@
+MONGODB_URL_KEY: str = "mongodb://ronak:1978@cluster0-shard-00-00.vtrl1.mongodb.net:27017,cluster0-shard-00-01.vtrl1.mongodb.net:27017,cluster0-shard-00-02.vtrl1.mongodb.net:27017/?ssl=true&replicaSet=atlas-w4rsak-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0"
+MONGO_DATABASE_NAME: str = "myntra-reviews"
+
+
+SESSION_PRODUCT_KEY: str = "product_name"
